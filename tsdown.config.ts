77937fd6017c2157d3823cdd9f771df3ab6a0017
6x68 <249649284+fun-fact-is-that-everything-after-the-plus-sign-is-useless@users.noreply.github.com>
2026-05-31 09:10:10 -0500

    fix: post dist folder instead of src on NPM

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,4 +3,7 @@ import { defineConfig, type UserConfig } from "tsdown";
 export default defineConfig({
   entry: "src/index.ts",
   dts: true,
+  platform: "neutral",
+  target: "esnext",
+  cjsDefault: false,
 }) satisfies UserConfig as UserConfig;
